fca092a9f8025e0fc585147231df65158742940f
Olga Nadongo <winnienadongo@gmail.com>
2022-06-03 13:49:53 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+
+const cats=["Milo", "Otis", "Garfield"]
+console.log(cats);
+
+ function destructivelyAppendCat(name){
+     console.log(cats.push("Ralph"))
+ }
+ function destructivelyPrependCat(name){
+    console.log(cats.unshift("Bob"))
+}
+function destructivelyRemoveLastCat(name){
+    console.log(cats.pop("Bob"))
+}
+function destructivelyRemoveFirstCat(name){
+    console.log(cats.shift())
+}
+
+function appendCat(name){
+  const newCats = [...cats]
+  newCats.push(name)
+    return newCats;
+}
+
+
+function prependCat(name){
+    const manyCats= [...cats];
+    manyCats.unshift(name)
+    return manyCats
+}
+
+function removeLastCat(name){
+    const fewCats=[...cats]
+    fewCats.pop(name)
+    return fewCats
+}
+
+function removeFirstCat(name){
+    const lessCats=[...cats]
+    lessCats.shift(name)
+    return lessCats
+}
+  
